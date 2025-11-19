da714a83ad9a6c5afd74675e397a71eaaefbb536
DynorBlue <dynorblue@gmail.com>
2025-11-19 15:12:34 -0600

    configuaracion de dependencias

--- app/src/main/java/org/utl/fitblueapp/MainActivity.kt
+++ app/src/main/java/org/utl/fitblueapp/MainActivity.kt
@@ -18,30 +18,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FitBlueAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FitBlueAppTheme {
-        Greeting("Android")
-    }
+
 }
